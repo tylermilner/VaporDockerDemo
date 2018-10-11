9d504030e7054db7f6ef0db9f121be3a8c2f73e0
Tyler Milner <tylermilner@gmail.com>
2018-10-10 19:07:54 -0500

    Dropped in Vapor API template app configured to work with PostgreSQL instead of the default SQLite.

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "VaporDockerDemo",
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ]
+)
---
+++ Sources/App/configure.swift
@@ -0,0 +1,33 @@
+import FluentPostgreSQL
+import Vapor
+
+/// Called before your application initializes.
+public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    /// Register providers first
+    try services.register(FluentPostgreSQLProvider())
+
+    /// Register routes to the router
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+
+    /// Register middleware
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    services.register(middlewares)
+
+    // Configure a PostgreSQL database
+    let databaseConfig = PostgreSQLDatabaseConfig(hostname: "127.0.0.1",
+                                                  port: 5432,
+                                                  username: "tylermilner",
+                                                  database: "vapordockerdemo",
+                                                  password: nil)
+    services.register(databaseConfig)
+
+    /// Configure migrations
+    var migrations = MigrationConfig()
+    migrations.add(model: Todo.self, database: .psql)
+    services.register(migrations)
+
+}
