608326a4af845c7bdeabc5512e8495ff2654fccc
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-10-11 16:47:22 -0500

    Stripped example down to just a basic Vapor "hello world" app.

--- Package.swift
+++ Package.swift
@@ -5,10 +5,9 @@ let package = Package(
     name: "VaporDockerDemo",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+        .target(name: "App", dependencies: ["Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,9 +3,6 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    /// Register providers first
-    try services.register(FluentPostgreSQLProvider())
-
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -16,18 +13,4 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
-    // Configure a PostgreSQL database
-    let databaseConfig = PostgreSQLDatabaseConfig(hostname: "0.0.0.0",
-                                                  port: 5432,
-                                                  username: "postgres",
-                                                  database: "postgres",
-                                                  password: "postgres")
-    services.register(databaseConfig)
-
-    /// Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .psql)
-    services.register(migrations)
-
 }
