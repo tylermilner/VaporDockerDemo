0119c47e5d559ed386fcc1b31461447634475572
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-10-22 17:03:46 -0500

    Re-implemented Todo list Vapor template app with the in-memory SQLite database so that differences between this branch and the 'master' branch are minimal (in terms of actual server app functionality).

--- Package.swift
+++ Package.swift
@@ -5,9 +5,10 @@ let package = Package(
     name: "VaporDockerDemo",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,8 +1,11 @@
-import FluentPostgreSQL
+import FluentSQLite
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    /// Register providers first
+    try services.register(FluentSQLiteProvider())
+
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -13,4 +16,18 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
+
+    // Configure a SQLite database
+    let sqlite = try SQLiteDatabase(storage: .memory)
+
+    /// Register the configured SQLite database to the database config.
+    var databases = DatabasesConfig()
+    databases.add(database: sqlite, as: .sqlite)
+    services.register(databases)
+
+    /// Configure migrations
+    var migrations = MigrationConfig()
+    migrations.add(model: Todo.self, database: .sqlite)
+    services.register(migrations)
+
 }
